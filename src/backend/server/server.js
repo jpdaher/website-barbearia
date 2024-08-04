6627f0bb83681c2fe2f729e74f24cb669367bc1b
João <csbernardo4@gmail.com>
2024-08-04 20:28:42 -0300

    serving html files

--- src/backend/server/server.js
+++ src/backend/server/server.js
@@ -3,8 +3,15 @@ const dotenv = require('dotenv')
 dotenv.config()
 const database = require('../database/database')
 const PORT = 8080
-
 const app = express()
+const filesDir = '../../frontend/html'
+app.use(express.static(filesDir))
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile('login.html', { root: filesDir })
+})
 
 app.get('/barbeiros', async (req, res) => {
     const barbeiros = await database.getBarbeiros()
