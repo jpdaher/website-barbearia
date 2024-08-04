762c98ef1bb8199a315b78b5314490519a8989fb
João <csbernardo4@gmail.com>
2024-08-04 19:48:13 -0300

    refactor

--- src/backend/server/server.js
+++ src/backend/server/server.js
@@ -1,10 +1,14 @@
-const express = require("express")
-const PORT = 5500
+const express = require('express');
+const dotenv = require('dotenv')
+dotenv.config()
+const database = require('../database/database')
+const PORT = 8080
 
 const app = express()
 
-app.get('/test', (req, res) => {
-    res.json({ ok: true })
+app.get('/barbeiros', async (req, res) => {
+    const barbeiros = await database.getBarbeiros()
+    res.json(barbeiros)
 })
 
-app.listen(PORT, () => console.log(`Server now listening on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server now listening on port http://localhost:${PORT}`))
